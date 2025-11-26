74f5a757ed3dcb4de1e129e7d493be15e898aa69
VmaWav <desarrollo.1@net2alliance.com>
2025-11-25 23:10:20 -0600

    feat(recovery_password): recovery_password page

--- pages/recovery_password.php
+++ pages/recovery_password.php
@@ -1,36 +1,32 @@
 <?php
 require_once __DIR__ . '/../config/app.php';
 $pageScript = "recovery_password.js";
+$pageStyle = "pages/recovery_password.css";
 ?>
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <?php require_once __DIR__ . '/../inc/inc_head.php'; ?>
-    <title>Recuperación de contraseña - Sales360</title>
-    <script>window.APP_BASE_URL = "<?= htmlspecialchars(BASE_URL ?: '') ?>";</script>
+    <title>Recuperación de contraseña - CPT</title>
 </head>
 <body>
-  <div id="recover-screen" class="login-container">
-    <div class="login-card">
-      <div class="login-header main-text-gradient">
-        <h1><i class="fas fa-key"></i> Recuperar Contraseña</h1>
+<div class="background-image">
+    <img src="<?= url('/img/login-bg.png') ?>" alt="Imagen edificio UPIIT hecha a mano" class="bg-image">
+  </div>
+  <div class="center-card-container">
+    <div class="recovery-card">
+      <p class="card-title">Recupera tu contraseña</p>
+      <p class="card-subtitle">Ingresa tu correo para recibir instrucciones</p>
+      <div class="form-group">
+        <label for="email">Correo electrónico</label>
+        <input type="text" id="email" class="form-control" placeholder="tu.correo@alumno.ipn.mx">
       </div>
-        <p class="login-subtitle">Ingresa tu correo electrónico para restablecer tu contraseña</p>
-      <div class="login-body">
-        <form prevent="submit">
-          <div class="form-group">
-            <label for="email">Correo Electrónico</label>
-            <input type="email" id="email" name="email" class="form-control" placeholder="Ingresa tu correo" required>
-          </div>
-          <button type="button" class="btn btn-primary" style="width: 100%; justify-content: center;" id="recovery-btn">
-            <i class="fas fa-paper-plane"></i> Enviar correo de recuperación
-          </button>
-        </form>
-      </div>
+      <button type="button" class="btn btn-primary" id="recovery-btn">
+        <i class="fas fa-sign-in-alt"></i> Enviar correo de recuperación
+      </button>
     </div>
   </div>
   <?php require_once __DIR__ . '/../inc/inc_footer_scripts.php'; ?>
+  <script src="../js/recovery_password.js"></script>
 </body>
-<script src="../js/recovery_password.js"/>
 </html>
