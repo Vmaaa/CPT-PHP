861a499b42636ac756ec4a779bd4b4e10431437b
VmaWav <desarrollo.1@net2alliance.com>
2025-11-25 23:19:17 -0600

    refactor(assets): sidebar & layout css files created

--- inc/inc_head.php
+++ inc/inc_head.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/../inc/inc_auth.php'; ?>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="<?= CSS_URL ?>/style.css">
 <link rel="stylesheet" href="<?= CSS_URL ?>/general.css">
+<link rel="stylesheet" href="<?= CSS_URL ?>/layout.css">
+<link rel="stylesheet" href="<?= CSS_URL ?>/components/sidebar.css">
 <link rel="stylesheet" href="<?= CSS_URL ?>/components/buttons.css">
 <link rel="stylesheet" href="<?= CSS_URL ?>/components/forms.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
