027e497046d2b96f432483a984246673097bf0c7
VmaWav <desarrollo.1@net2alliance.com>
2025-11-25 22:12:44 -0600

    refactor(inc_head): obtain pageStyle variable to load a specific css file

--- inc/inc_head.php
+++ inc/inc_head.php
@@ -5,6 +5,11 @@ require_once __DIR__ . '/../inc/inc_auth.php'; ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="<?= CSS_URL ?>/style.css">
+<link rel="stylesheet" href="<?= CSS_URL ?>/general.css">
+<link rel="stylesheet" href="<?= CSS_URL ?>/buttons.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 <script defer src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<?php if (!empty($pageStyle)): ?>
+    <link rel="stylesheet" href="<?= CSS_URL . '/' . basename($pageStyle) ?>">
+<?php endif; ?>
